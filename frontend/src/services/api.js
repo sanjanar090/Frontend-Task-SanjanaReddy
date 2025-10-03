1a9111ddfebdf46d1ca3c6631f46eccc2a0ec76c
sanjana090 <rsanjana090@gmail.com>
2025-10-03 13:03:57 +0530

    updated project and screenshots

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,12 +4,19 @@ const API = axios.create({
   baseURL: "http://127.0.0.1:8000/api/",
 });
 
-// 🔑 Attach token automatically
+// 🔑 Attach token only for protected routes
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) {
+
+  // Don't attach token for register/login
+  if (
+    token &&
+    !config.url.includes("register") &&
+    !config.url.includes("login")
+  ) {
     config.headers.Authorization = `Token ${token}`;
   }
+
   return config;
 });
 
